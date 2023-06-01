edbeb5469df1bb12315eaa3fe869e72c0f6654a4
yh9035926 <yh9035926@naver.com>
2023-06-02 07:16:46 +0900

    todolist 완료

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -137,10 +137,3 @@ const Home = styled(Link)`
     animation: ${jump} 0.2s ease-out forwards;
   }
 `;
-
-const Title = styled.h2`
-  font-weight: normal;
-  color: #2a2a29;
-  text-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
-  text-align: center;
-`;
