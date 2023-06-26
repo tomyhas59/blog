5c54d28bffe99a47daef68b895dff4ba17318248
yh9035926 <yh9035926@naver.com>
2023-06-26 12:50:05 +0900

    쿠키 주고받아야 req.user가 담긴다

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ dotenv.config();
 app.use(
   cors({
     origin: "http://localhost:3000",
-    credentials: true, //쿠키 보내는 코드 프론트의 saga/index에서 axios.defaults.withCredentials = true 해줘야 쿠키 받음
+    credentials: true, //쿠키 보내는 코드, 프론트의 saga/index에서 axios.defaults.withCredentials = true 해줘야 쿠키 받음
   })
 );
 app.use(cookieParser());
