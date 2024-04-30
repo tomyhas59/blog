17708db03fe64969cf9ce31752beeb1a03763ab5
yh9035926 <yh9035926@naver.com>
2024-04-30 10:15:10 +0900

    post?

--- src/components/Post.js
+++ src/components/Post.js
@@ -214,7 +214,7 @@ const Post = ({ post, imagePaths }) => {
                 onClick={handlePopupToggle}
                 ref={nicknameButtonRef}
               >
-                {post.User.nickname || null}
+                {/*   {post.User.nickname} */}
               </NicknameButton>
               {showPopup && (
                 <PopupMenu ref={popupRef}>
