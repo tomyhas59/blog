adfeed4e6392a3bcc94ae59e4838918e93dffb14
이용현 <yh9035926@naver.com>
2024-05-05 03:19:50 +0900

    cloudtype

--- src/saga/index.ts
+++ src/saga/index.ts
@@ -5,7 +5,7 @@ import userSaga from "./user";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:8000"
+    ? "https://port-0-blog-server-rccln2llvsdixmg.sel5.cloudtype.app"
     : "http://localhost:3075";
 axios.defaults.withCredentials = true;
 //백에서 쿠키 받음,
