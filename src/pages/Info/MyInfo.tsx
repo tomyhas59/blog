4ccd0e4be9227e7536b0a7cadd510e841cd997d3
이용현 <yh9035926@naver.com>
2024-07-11 08:33:52 +0900

    createdAt

--- src/pages/Info/MyInfo.tsx
+++ src/pages/Info/MyInfo.tsx
@@ -1,13 +1,49 @@
-// MyInfo.tsx
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../reducer";
+import moment from "moment";
+import styled from "styled-components";
 
 const MyInfo: React.FC = () => {
+  const { me } = useSelector((state: RootState) => state.user);
+
+  if (!me) {
+    return <div>사용자 정보를 불러오는 중입니다...</div>;
+  }
+  const createdAt = me.createdAt;
+  console.log("me", me);
+  const createdAtDate = moment(createdAt);
+  const formattedDate = createdAtDate.format("l");
+
   return (
-    <div>
-      <h2>내 정보</h2>
-      <p>여기에 사용자의 정보를 표시합니다.</p>
-    </div>
+    <InfoContainer>
+      <h1>내 정보</h1>
+      <UserInfo>
+        <strong>사용자명:</strong> {me.nickname}
+      </UserInfo>
+      <UserInfo>
+        <strong>이메일:</strong> {me.email}
+      </UserInfo>
+      <UserInfo>
+        <strong>가입일:</strong> {formattedDate}
+      </UserInfo>
+    </InfoContainer>
   );
 };
 
 export default MyInfo;
+
+const InfoContainer = styled.div`
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 20px;
+  h1 {
+    font-weight: bold;
+  }
+`;
+
+const UserInfo = styled.p`
+  margin: 10px 0;
+`;
