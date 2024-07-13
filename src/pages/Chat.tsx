0cabd078c2d26e5a97c5687508e5b50d8909f5db
yh9035926 <yh9035926@naver.com>
2024-07-13 10:05:03 +0900

    AllChatRoom

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -5,6 +5,7 @@ import React, {
   ChangeEvent,
   useRef,
   useCallback,
+  RefObject,
 } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
@@ -18,11 +19,73 @@ import {
   DELETE_ALL_CHAT_REQUEST,
   READ_CHAT_REQUEST,
 } from "../reducer/post";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 const socket =
   process.env.NODE_ENV === "production"
     ? io("https://quarrelsome-laura-tomyhas59-09167dc6.koyeb.app")
     : io("http://localhost:3075");
+
+interface AllChatRoomType {
+  messages: Message[];
+  me: UserType | null;
+  onDeleteAllMessages: () => void;
+  onMessageSubmit: (e: SyntheticEvent) => void;
+  inputValue: string;
+  messageRef: RefObject<HTMLInputElement>;
+  onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const AllChatRoom = ({
+  messages,
+  me,
+  onDeleteAllMessages,
+  onMessageSubmit,
+  inputValue,
+  messageRef,
+  onInputChange,
+}: AllChatRoomType) => (
+  <>
+    <ChatHeader>단체 채팅방</ChatHeader>
+    {me?.nickname === "admin" && (
+      <button onClick={onDeleteAllMessages}>모든 대화 삭제</button>
+    )}
+    <MessageList>
+      {messages.map((message, i) => (
+        <React.Fragment key={message.id}>
+          {i === 0 ||
+          moment(message.createdAt).isAfter(
+            messages[i - 1].createdAt,
+            "day"
+          ) ? (
+            <DateSeparator>
+              {moment(message.createdAt).format("YYYY-MM-DD")}
+            </DateSeparator>
+          ) : null}
+          <MessageItem key={message.id} isMe={message.sender === me?.nickname}>
+            <MessageSender>{message.sender.slice(0, 5)}</MessageSender>
+            <MessageText isMe={message.sender === me?.nickname}>
+              {message.content}
+            </MessageText>
+            <MessageTime>
+              {moment(message.createdAt).format("HH:mm")}
+            </MessageTime>
+          </MessageItem>
+        </React.Fragment>
+      ))}
+    </MessageList>
+    <MessageForm onSubmit={onMessageSubmit}>
+      <MessageInput
+        type="text"
+        placeholder="메시지를 입력해주세요"
+        value={inputValue}
+        ref={messageRef}
+        onChange={onInputChange}
+      />
+      <MessageButton type="submit">전송</MessageButton>
+    </MessageForm>
+  </>
+);
 
 const Chat = () => {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -30,6 +93,7 @@ const Chat = () => {
   const { chatMessages } = useSelector((state: RootState) => state.post);
   const { me } = useSelector((state: RootState) => state.user);
   const [userList, setUserList] = useState<UserType[] | null>(null);
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
   const messageRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
 
@@ -97,56 +161,94 @@ const Chat = () => {
     });
   }, [dispatch]);
 
+  const [userOption, setUserOption] = useState<boolean>(false);
+
+  const onUserOption = (nickname: string) => {
+    setSelectedUser(nickname); // Set selected user's nickname
+    setUserOption((prev) => !prev);
+  };
+
+  const userOptoinRef = useRef<HTMLDivElement>(null);
+
+  useOutsideClick([userOptoinRef], () => {
+    setUserOption(false);
+  });
+
+  const [activeRoom, setActiveRoom] = useState("allChat");
+  const [roomList, setRoomList] = useState<string[]>([]);
+
+  const renderRoom = () => {
+    switch (activeRoom) {
+      case "allChat":
+        return (
+          <AllChatRoom
+            messages={messages}
+            me={me}
+            onDeleteAllMessages={onDeleteAllMessages}
+            onMessageSubmit={onMessageSubmit}
+            inputValue={inputValue}
+            messageRef={messageRef}
+            onInputChange={onInputChange}
+          />
+        );
+      case selectedUser:
+        return (
+          <>
+            <ChatHeader>{selectedUser}님과의 1:1 채팅방</ChatHeader>
+          </>
+        );
+      default:
+        return (
+          <AllChatRoom
+            messages={messages}
+            me={me}
+            onDeleteAllMessages={onDeleteAllMessages}
+            onMessageSubmit={onMessageSubmit}
+            inputValue={inputValue}
+            messageRef={messageRef}
+            onInputChange={onInputChange}
+          />
+        );
+    }
+  };
+
   return (
     <ChatContainer>
-      <ChatWrapper>
-        <ChatHeader>단체 채팅방</ChatHeader>
-        {me?.nickname === "admin" && (
-          <button onClick={onDeleteAllMessages}>모든 대화 삭제</button>
-        )}
-        <MessageList>
-          {messages.map((message, i) => (
-            <React.Fragment key={message.id}>
-              {i === 0 ||
-              moment(message.createdAt).isAfter(
-                messages[i - 1].createdAt,
-                "day"
-              ) ? (
-                <DateSeparator>
-                  {moment(message.createdAt).format("YYYY-MM-DD")}
-                </DateSeparator>
-              ) : null}
-              <MessageItem
-                key={message.id}
-                isMe={message.sender === me?.nickname}
-              >
-                <MessageSender>{message.sender.slice(0, 5)}</MessageSender>
-                <MessageText isMe={message.sender === me?.nickname}>
-                  {message.content}
-                </MessageText>
-                <MessageTime>
-                  {moment(message.createdAt).format("HH:mm")}
-                </MessageTime>
-              </MessageItem>
-            </React.Fragment>
-          ))}
-        </MessageList>
-        <MessageForm onSubmit={onMessageSubmit}>
-          <MessageInput
-            type="text"
-            placeholder="메시지를 입력해주세요"
-            value={inputValue}
-            ref={messageRef}
-            onChange={onInputChange}
-          />
-          <MessageButton type="submit">전송</MessageButton>
-        </MessageForm>
-      </ChatWrapper>
+      <RoomList>
+        <RoomItem>
+          <button onClick={() => setActiveRoom("allChat")}>전체 채팅방</button>
+        </RoomItem>
+        {roomList.map((room) => (
+          <RoomItem>
+            <button onClick={() => setActiveRoom(room)}>{room}방</button>
+          </RoomItem>
+        ))}
+      </RoomList>
+
+      <ChatWrapper>{renderRoom()}</ChatWrapper>
       <UserList>
         <div>{userList?.length || 0}명 접속 중</div>
         <ul>
           {userList?.map((user) => (
-            <li key={user.id}>{user.nickname.slice(0, 5)}</li>
+            <li key={user.id}>
+              <button onClick={() => onUserOption(user.nickname)}>
+                {user.nickname.slice(0, 5)}
+              </button>
+              {userOption && selectedUser === user.nickname && (
+                <UserOption ref={userOptoinRef}>
+                  <button
+                    onClick={() => {
+                      setActiveRoom(user.nickname);
+                      setUserOption(false);
+                      setRoomList((prev) => [...prev, user.nickname]);
+                    }}
+                  >
+                    1:1 채팅하기
+                  </button>
+                  <button>팔로우</button>
+                </UserOption>
+              )}
+            </li>
           ))}
         </ul>
       </UserList>
@@ -164,6 +266,20 @@ const ChatContainer = styled.div`
   }
 `;
 
+const RoomList = styled.div`
+  width: 200px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  margin-right: 20px;
+  text-align: center;
+`;
+
+const RoomItem = styled.div`
+  padding: 5px;
+  margin-bottom: 5px;
+  background-color: #ccc;
+`;
+
 const ChatWrapper = styled.div`
   width: 600px;
   padding: 20px;
@@ -177,12 +293,13 @@ const ChatWrapper = styled.div`
 const UserList = styled.div`
   color: ${(props) => props.theme.mainColor};
   font-size: 24px;
-  margin-left: 100px;
+  margin-left: 20px;
   > ul {
     display: flex;
     flex-direction: column;
     align-items: center;
     > li {
+      position: relative;
       &:hover {
         cursor: pointer;
         text-decoration: underline;
@@ -194,6 +311,23 @@ const UserList = styled.div`
     position: absolute;
     > ul {
       flex-direction: row;
+    }
+  }
+`;
+
+const UserOption = styled.div`
+  left: 20px;
+  font-size: 12px;
+  position: absolute;
+  background-color: ${(props) => props.theme.mainColor};
+  border-radius: 5px;
+  color: #fff;
+  text-align: center;
+  & button {
+    width: 70px;
+    margin: 10px;
+    &:hover {
+      color: ${(props) => props.theme.charColor};
     }
   }
 `;
