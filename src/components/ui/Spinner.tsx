214d388930bb0b99237762260c98e06db8078d94
이용현 <yh9035926@naver.com>
2025-06-17 18:38:34 +0900

    spinner mid

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -15,7 +15,7 @@ const SpinnerContainer = styled.div`
   position: fixed;
   left: 50%;
   top: 50%;
-  transform:translate(-50%,-50%)
+  transform: translate(-50%, -50%);
 `;
 
 export default Spinner;
