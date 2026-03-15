aa73d2bf437283da4704a3d9365e20aa5684f268
tomyhas59 <yh9035926@naver.com>
2026-03-15 20:30:51 +0900

    Fix ESLint

--- src/hooks/useHorizontalScroll.ts
+++ src/hooks/useHorizontalScroll.ts
@@ -2,19 +2,24 @@ import { useEffect } from "react";
 
 const useHorizontalScroll = (ref: React.RefObject<HTMLElement>) => {
   useEffect(() => {
-    if (!ref.current) return;
+    const element = ref.current; // 1. ref.current를 변수에 복사
 
-    const el = ref.current;
+    if (element) {
+      const onWheel = (e: WheelEvent) => {
+        if (e.deltaY === 0) return;
+        e.preventDefault();
+        element.scrollTo({
+          left: element.scrollLeft + e.deltaY,
+          behavior: "smooth",
+        });
+      };
 
-    const handleWheel = (e: WheelEvent) => {
-      if (e.deltaY === 0) return;
-      e.preventDefault();
-      el.scrollLeft += e.deltaY;
-    };
+      element.addEventListener("wheel", onWheel);
+      return () => element.removeEventListener("wheel", onWheel); // 2. 복사한 변수로 클린업
+    }
+  }, [ref]); // 3. 이제 ref를 넣어줘도 ESLint가 안심합니다.
 
-    el.addEventListener("wheel", handleWheel, { passive: false });
-    return () => el.removeEventListener("wheel", handleWheel);
-  }, [ref.current]);
+  return ref;
 };
 
 export default useHorizontalScroll;
